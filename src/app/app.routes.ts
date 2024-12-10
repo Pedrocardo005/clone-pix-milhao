4c926bfc3efb0e3dcbce19eae49c364d0123b6dd
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-12-10 19:13:06 -0300

    :sparkles: feat: Add rota checkout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { CheckoutComponent } from './features/pix/pages/checkout/checkout.component';
 import { HomeComponent } from './features/pix/pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
   },
 ];
---
+++ src/app/features/pix/pages/checkout/checkout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  imports: [],
+  standalone: true,
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css',
+})
+export class CheckoutComponent {}
