b5d9abf062d007f953d55ff0e3841f22085c4026
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-12-10 07:41:52 -0300

    :lipstick: feat: Add textos pagina inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         extend: {
             backgroundImage: {
                 'custom-gradient': 'linear-gradient(450deg, #000 0%, #454545 100%)',
+                'button-buy-gradient': 'linear-gradient(90deg, #008a20 0%, #a2dfad 100%)',
             }
         },
     },
