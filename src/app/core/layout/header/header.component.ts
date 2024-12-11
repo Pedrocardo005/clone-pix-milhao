d8e92bf292bba2d1f289397ee00e061bf1b08c7b
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-12-11 07:50:02 -0300

    :iphone: feat: Add responsividade

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isMobile = false;
+
+  ngOnInit() {
+    if (window.screen.width < 400) {
+      this.isMobile = true;
+    }
+  }
+}
